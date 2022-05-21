6bb40fbdbe07fbe006446990470cc41dc9f7eeed
Aleksandr P <Morwar211@gmail.com>
2022-05-21 11:08:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,84 +1,15 @@
-import requests
-import csv
+j = input()
+s = input()
 
 
-def request_posts(domain: str) -> list:
-    token = "token"
-    version = 5.131
-    offset = 0
-    count = 200
-    filter = 'all'
-
-    response = requests.get("http://api.vk.com/method/wall.get",
-                            params={
-                                'access_token': token,
-                                'v': version,
-                                'domain': domain,
-                                'count': count,
-                                'filter': filter
-                            })
-
-    data = response.json()['response']['items']
-    data_list = list()
-    data_list.extend(data)
-    print(data_list)
-    return data_list
+def count_overlapping_substrings(haystack, needle):
+    count = 0
+    i = -1
+    while True:
+        i = haystack.find(needle, i + 1)
+        if i == -1:
+            return count
+        count += 1
 
 
-def output(data_list: list):
-    with open('Analyze.csv', 'w') as file:
-        writer = csv.writer(file)
-        writer.writerow(['likes', 'reposts', 'body', 'url'])
-        img_url = ""
-        for post in data_list:
-            try:
-                if post['attachments'][0]['type']:
-                    img_url = post['attachments'][0]['photo']['sizes'][-1]['url']
-                else:
-                    img_url = 'pass'
-            except:
-                pass
-            try:
-                writer.writerow([post['likes']['count'], post['reposts']['count'], post['text'], img_url])
-            except:
-                pass
-
-
-def counter(data_list: list):
-    i = 0
-    likes_sum = 0
-    reposts_sum = 0
-
-    for post in data_list:
-        if i == 0:
-            last_post = post['text']
-        else:
-            continue
-        likes_sum += post['likes']['count']
-        reposts_sum += post['reposts']['count']
-        i += 1
-
-    if last_post != '':
-        pass
-    else:
-        last_post = "contains no text."
-
-    print("Amount of likes for 300 posts = " + str(likes_sum) + ";" +
-          "\nAmount of reposts for 300 posts = " + str(reposts_sum) + ";" +
-          "\nAVG likes per post = " + str(likes_sum / i) + ";" +
-          "\nAVG reposts per post = " + str(reposts_sum / i) + "."
-                                                               "\n***************************\n" +
-          '\nLast post:''\n\t"' + str(last_post) + '"'
-          )
-
-
-while True:
-    print("\n***************************")
-    print("Hello, input VK public URL: ")
-    domain = str(input())
-    try:
-        data_list = request_posts(domain)
-        output(data_list)
-        counter(data_list)
-    except:
-        print("ERROR:\tOops an incorrect domain name \n")
+print(count_overlapping_substrings(s, j))
